1d6a95fbf48d924c9221b6c44fba806f1182654f
julia8024 <julia8024@naver.com>
2024-06-29 17:13:49 +0900

    Feat: UserDefaultManager 클래스 추가

---
+++ VelogLog/UserDefaultManager.swift
@@ -0,0 +1,30 @@
+//
+//  UserDefaultManager.swift
+//  VelogLog
+//
+//  Created by 장세희 on 6/29/24.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+    enum UserDefaultsKeys: String, CaseIterable {
+        case userId
+    }
+    
+    static func setData<T>(value: T, key: UserDefaultsKeys) {
+        let defaults = UserDefaults.standard
+        defaults.set(value, forKey: key.rawValue)
+    }
+    
+    static func getData<T>(type: T.Type, forKey: UserDefaultsKeys) -> T? {
+        let defaults = UserDefaults.standard
+        let value = defaults.object(forKey: forKey.rawValue) as? T
+        return value
+    }
+    
+    static func removeData(key: UserDefaultsKeys) {
+        let defaults = UserDefaults.standard
+        defaults.removeObject(forKey: key.rawValue)
+    }
+}
