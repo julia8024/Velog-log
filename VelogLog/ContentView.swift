ffec8cf44c5a86967b44ae00546de33b17c77e75
julia8024 <julia8024@naver.com>
2024-06-30 21:47:03 +0900

    Fix: 수정 불러오는 글의 제한 6 -> 100

--- VelogLog/ContentView.swift
+++ VelogLog/ContentView.swift
@@ -102,7 +102,7 @@ struct ContentView: View {
             "operationName":"Posts",
             "variables": [
                 "username": userId,
-                "limit": 6
+                "limit": 100
             ],
             "query":"query Posts($cursor: ID, $username: String, $temp_only: Boolean, $tag: String, $limit: Int) {\n  posts(cursor: $cursor, username: $username, temp_only: $temp_only, tag: $tag, limit: $limit) {\n    id\n    title\n    short_description\n    thumbnail\n    user {\n      id\n      username\n      profile {\n        id\n        thumbnail\n        __typename\n      }\n      __typename\n    }\n    url_slug\n    released_at\n    updated_at\n    comments_count\n    tags\n    is_private\n    likes\n    __typename\n  }\n}\n"
         ]
