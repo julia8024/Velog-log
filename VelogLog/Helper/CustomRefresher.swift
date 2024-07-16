9591054c315303a6f733a3a22d58f8bf0f5b7802
julia8024 <julia8024@naver.com>
2024-07-17 01:12:09 +0900

    Feat: Add Rotate Animation in Refresh Button

--- VelogLog/Helper/CustomRefresher.swift
+++ VelogLog/Helper/CustomRefresher.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CustomRefresher: View {
     @Environment(\.refresh) private var refresh
     
+    @State private var rotationAngle: Double = 0
+    
     var body: some View {
         Group {
             if let refresh = refresh {
@@ -17,6 +19,14 @@ struct CustomRefresher: View {
                     action: {
                         Task {
                             await refresh()
+                            withAnimation {
+                                // 현재 회전 각도에 360을 더하여 오른쪽 방향으로 회전
+                                rotationAngle += 360
+                            }
+                            // 회전 후에 0으로 초기화 (값이 너무 커지지 않도록)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                rotationAngle = 0
+                            }
                         }
                     },
                     label: {
@@ -24,6 +34,7 @@ struct CustomRefresher: View {
                             Image(systemName: "arrow.clockwise")
                                 .foregroundStyle(Color("DefaultTextColor"))
                                 .font(.system(size: 20))
+                                .rotationEffect(.degrees(rotationAngle))
                         }
                         .padding(15)
                         .background(Color("DefaultColor"))
@@ -34,3 +45,4 @@ struct CustomRefresher: View {
         }
     }
 }
+
