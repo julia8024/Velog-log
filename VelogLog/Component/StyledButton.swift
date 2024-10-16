0482362f846286e5329d4424274316bc333c6a02
julia8024 <julia8024@naver.com>
2024-10-16 22:13:38 +0900

    Feat: add StyledButton Component

---
+++ VelogLog/Component/StyledButton.swift
@@ -0,0 +1,25 @@
+//
+//  StyledButton.swift
+//  VelogLog
+//
+//  Created by 장세희 on 10/16/24.
+//
+
+import SwiftUI
+
+struct StyledButton: View {
+    
+    let text: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(text)
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color("DefaultTextColor"))
+                .cornerRadius(8)
+        }
+    }
+}
